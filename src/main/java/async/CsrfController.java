cb1abf29029fcbe19b3841be5bffc4944930ea0f
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-10-17 19:11:09 +0800

    Update CsrfController.java

--- src/main/java/async/CsrfController.java
+++ src/main/java/async/CsrfController.java
@@ -5,25 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CsrfController {
 
     @GetMapping("/csrf-token")
-    public Map<String, String> getCsrfToken(HttpServletRequest request) {
-        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-        Map<String, String> tokenMap = new HashMap<>();
-        
-        if (csrfToken != null) {
-            tokenMap.put("token", csrfToken.getToken());
-        } else {
-            // 返回适当的错误响应或抛出异常
-            throw new IllegalStateException("CSRF token not found");
-        }
-        
-        return tokenMap;
+    public CsrfToken csrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
     }
 }
-
