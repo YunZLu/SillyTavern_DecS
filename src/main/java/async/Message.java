039e0267c5c190f85afed27b4c017a5362a4d6c8
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-10-18 11:09:14 +0800

    Update Message.java

--- src/main/java/async/Message.java
+++ src/main/java/async/Message.java
@@ -1,23 +1,27 @@
 package async;
 
 public class Message {
-    private String role;
-    private String content;
+    private String content;  // 内容字段，存储需要解密或处理的消息内容
+
+    // 构造函数
+    public Message(String content) {
+        this.content = content;
+    }
 
     // Getter 和 Setter 方法
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
     public String getContent() {
         return content;
     }
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    // 增加 toString() 方法便于日志输出
+    @Override
+    public String toString() {
+        return "Message{" +
+                "content='" + content + '\'' +
+                '}';
     }
 }
