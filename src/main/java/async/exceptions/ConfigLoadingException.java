41a7a6534b1167ecb38503f7a790542345152a93
YunZLu <130174259+YunZLu@users.noreply.github.com>
2024-10-18 17:02:18 +0800

    Delete src/main/java/async/exceptions directory

--- src/main/java/async/exceptions/ConfigLoadingException.java
+++
@@ -1,7 +0,0 @@
-package async.exceptions;
-
-public class ConfigLoadingException extends RuntimeException {
-    public ConfigLoadingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/async/exceptions/DecryptionException.java
+++
@@ -1,7 +0,0 @@
-package async.exceptions;
-
-public class DecryptionException extends RuntimeException {
-    public DecryptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
